9f1352ae0fce93d5e8ce65783b774b68e819cfce
erika.tokumoto <>
2018-06-14 01:46:38 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,5 @@
 <?php
 Route::get('/', 'WelcomeController@index');
 
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup','Auth\RegisterController@register')->name('signup.post');
